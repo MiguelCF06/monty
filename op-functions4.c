aec92f7ae45c2625f08618417f78d506cde2f0bb
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-14 02:49:49 +0000

    Fixed betty

--- op-functions4.c
+++ op-functions4.c
@@ -12,7 +12,7 @@ void rotl(stack_t **stack, unsigned int line_number)
 	if (!stack || !*stack || !(*stack)->next)
 		return;
 
-	*stack->next->prev = NULL;
+	(*stack)->next->prev = NULL;
 	while (temp && temp->next)
 		temp = temp->next;
 
