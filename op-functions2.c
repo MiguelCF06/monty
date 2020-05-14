cd563feee6f218c4302b9687a476a5698732040e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-14 00:01:00 +0000

    [Updated]Added function pint

---
+++ op-functions2.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ *pint - Prints the value at the top of the stack
+ *@stack: The doubly linked list
+ *@line_number: Line of the text
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack && stack)
+		printf("%d\n", (*stack)->n);
+	else
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
