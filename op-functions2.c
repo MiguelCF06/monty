c1c453d8bd86f53509c37139ac1a45d7bec5c5ca
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-14 00:18:00 +0000

    [Updated]Changed the pint function

--- op-functions2.c
+++ op-functions2.c
@@ -6,13 +6,20 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack && stack)
-		printf("%d\n", (*stack)->n);
-	else
+	if (!stack)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		freeAll(stack);
 		exit(EXIT_FAILURE);
 	}
+	printf("%d\n", (*stack)->n);
+}
+/**
+ *pop - Removes the top element of the stack
+ *@stack: The doubly linked list
+ *@line_number: Line of the text
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
 	
 }
